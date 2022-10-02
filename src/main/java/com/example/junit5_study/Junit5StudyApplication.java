2a6a81d102969e015145840bcfc39d08ed834d83
jji3379 <jji3379@naver.com>
2022-10-03 01:49:35 +0900

    Initial commit

---
+++ src/main/java/com/example/junit5_study/Junit5StudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.junit5_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Junit5StudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/junit5_study/Junit5StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junit5_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
